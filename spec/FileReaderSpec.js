55dd81ea0dc1257cc972b43275e40cdf4d68e588
John Ashton <John_ashton@hotmail.com>
2017-10-01 08:04:01 +0100

    Tests passing

--- spec/FileReaderSpec.js
+++ spec/FileReaderSpec.js
@@ -1,19 +1,19 @@
-describe('FileReader', function() {
+describe("FileReader", function() {
 
   beforeEach(function() {
-    var filereader = new FileReader();
+     filereader = new FileReader();
   });
 
   describe("Default FileReader", function(){
     it('has an empty array upon creation', function(){
-      expect(filereader.wordStore()).toEqual([])
+      expect(filereader.wordStore).toEqual([])
     });
   });
 
   describe("Reading words", function(){
     it('can sort individual words', function (){
       filereader.fileWordOccurances('Hello my name is test')
-      expect(filereader.wordStore()).length.toBe(5)
-    })
-  })
+      expect(filereader.wordStore).toEqual([ 'hello', 'is', 'my', 'name', 'test' ])
+    });
+  });
 });
