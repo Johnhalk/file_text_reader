2fc83b6b8e1d4a81c268cf14f4a8dae2e58d843f
John Ashton <John_ashton@hotmail.com>
2017-10-02 11:59:52 +0100

    Can sort word array into key value pairs of frequency they occur

--- spec/FileReaderSpec.js
+++ spec/FileReaderSpec.js
@@ -1,19 +1,26 @@
-describe("FileReader", function() {
+describe("FileReader", () => {
 
-  beforeEach(function() {
+  beforeEach( () =>{
      filereader = new FileReader();
   });
 
-  describe("Default FileReader", function(){
-    it('has an empty array upon creation', function(){
+  describe("Default FileReader", () =>{
+    it('has an empty array upon creation', () =>{
       expect(filereader.wordStore).toEqual([])
     });
   });
 
   describe("Reading words", function(){
-    it('can sort individual words', function (){
+    it('can read individual words', function (){
       filereader.fileWordOccurances('Hello my name is test')
       expect(filereader.wordStore).toEqual([ 'hello', 'is', 'my', 'name', 'test' ])
+    });
+  });
+
+  describe("Sorting words into frequency", function(){
+    it('can sort words and show how often they occur', function(){
+      filereader.sortWordFrequency(["greetings", "i", "am", "olaf", "olaf", "i", "am", "and", "i", "say", "greetings"])
+      expect(filereader.wordFrequency).toEqual({greetings: 2, i: 3, am: 2, olaf: 2, and: 1, say: 1})
     });
   });
 });
--- src/FileReader.js
+++ src/FileReader.js
@@ -1,8 +1,16 @@
 function FileReader() {
   this.wordStore = []
+  this.wordFrequency = {}
 };
 
 // Format input to store words in an array
 FileReader.prototype.fileWordOccurances = function(file) {
   this.wordStore = file.toString().toLowerCase().replace(/[^a-z\d\s]/ig,'').split(" ").sort()
+}
+
+FileReader.prototype.sortWordFrequency = function(wordArray) {
+  for (var i = 0; i < wordArray.length; i++) {
+    var num = wordArray[i];
+    this.wordFrequency[num] = this.wordFrequency[num] ? this.wordFrequency[num] + 1 : 1;
+  }
 }
